3db5d3212275a6a87b3cdf35cb95ff4bc690c98c
HunnyJ434 <fhunny@lakeheadu.ca>
2025-01-21 22:45:47 -0500

    first commit

--- schemaTypes/documents/collections-braids.ts
+++ schemaTypes/documents/collections-braids.ts
@@ -1,36 +1,41 @@
 import { defineType } from 'sanity';
 
 export const braids = defineType({
-    name: 'braids',
-    title: 'Braids',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: { hotspot: true },
-      },
-      {
-        name: 'discount',
-        title: 'Discount',
-        type: 'number',
-      },
-      {
-        name: 'fromPrice',
-        title: 'Price',
-        type: 'number',
-      },
-      {
-        name: 'oldPrice',
-        title: 'Old Price',
-        type: 'number',
-      },
-    ],
-  });
-  
+  name: 'braids',
+  title: 'Braids',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: { hotspot: true },
+    },
+    {
+      name: 'discount',
+      title: 'Discount',
+      type: 'number',
+    },
+    {
+      name: 'fromPrice',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'oldPrice',
+      title: 'Old Price',
+      type: 'number',
+    },
+    {
+      name: 'colors',
+      title: 'Colors',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+  ],
+});
